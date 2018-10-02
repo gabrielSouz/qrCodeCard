e410dd4584045537172749d9896b161433441581
gabriel souza de andrade <ff7gab@gmail.com>
2018-10-02 09:16:12 -0300

    inputIMGbackground activity Code Better ok

--- app/src/main/java/com/example/gabriel/qrcode/InputImgBackGroundURI.java
+++ app/src/main/java/com/example/gabriel/qrcode/InputImgBackGroundURI.java
@@ -13,15 +13,19 @@ import android.widget.ImageView;
 
 import java.io.IOException;
 
-import static com.example.gabriel.qrcode.MainActivity.TAG;
 
-public class InputImgBackGroundURI extends Activity implements View.OnClickListener{
+
+public class InputImgBackGroundURI extends Activity {
     public static final int PICK_IMAGE = 1;
-    private Button botao;
+    private Button btnChooseIMG;
     private ImageView imgBack;
-    private Button botaoOk;
-    public static Uri uriAtrr = null;
-    Bitmap bitmap = null;
+    private Button btnOK;
+
+    //Vars to persiste to btnOK which send data do next activity
+    Bitmap imgBitmap;
+    Uri imgUri;
+
+
 
 
 
@@ -29,17 +33,34 @@ public class InputImgBackGroundURI extends Activity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imagem_back_ground);
-        botao = (Button)findViewById(R.id.buttonPick);
-        botaoOk =(Button) findViewById(R.id.buttonOK);
-        imgBack = (ImageView) findViewById(R.id.imgBack);
-        botao.setOnClickListener(this);
-        botaoOk.setOnClickListener(new View.OnClickListener() {
+        btnChooseIMG = findViewById(R.id.buttonPick);
+        btnOK = findViewById(R.id.buttonOK);
+        imgBack =  findViewById(R.id.imgBack);
+        configBtns();
+
+    }
+
+
+    private void configBtns(){
+        btnChooseIMG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+            }
+        });
+
+
+
+        btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i1 = new Intent(InputImgBackGroundURI.this,InputSite.class);
-                i1.setData(uriAtrr);
-                i1.putExtra("imgH",bitmap.getHeight());
-                i1.putExtra("imgW",bitmap.getWidth());
+                i1.setData(imgUri);
+                i1.putExtra("imgH",imgBitmap.getHeight());
+                i1.putExtra("imgW",imgBitmap.getWidth());
                 startActivity(i1);
 
 
@@ -52,28 +73,21 @@ public class InputImgBackGroundURI extends Activity implements View.OnClickListe
     public void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         if (requestCode == PICK_IMAGE) {
-            Uri uri = data.getData();
-
+            imgUri = data.getData();
+            imgBitmap = null;
 
             try {
-               bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
-               botaoOk.setEnabled(true);
+                imgBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imgUri);
+               btnOK.setEnabled(true);
             }catch (IOException ex) {
 
             }
-            imgBack.setImageBitmap(bitmap);
-            uriAtrr = uri;
+            imgBack.setImageBitmap(imgBitmap);
+
 
         }
     }
 
 
-    @Override
-    public void onClick(View view) {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
 
-    }
 }
